d591c0c56ad8ce1954a40a29d220f231d2cf6049
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-03-26 19:01:40 +0100

    adding more answers to exercises 1 to 20.

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -11,6 +11,7 @@ print(my_listComprehension)
 import math
 import os
 import random
+import sys
 """
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
@@ -83,7 +84,6 @@ print(capital_letters)
 consonants = [e for e in tqbf if e not in 'aeiouAEIOU ']
 print(consonants)
 
-"""
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
@@ -92,11 +92,15 @@ print(consonants)
 path = '../../..'
 files = [e.name for e in os.scandir(path) if (e.is_dir() and not e.name.startswith('.'))]
 print(files)
-# @@@@@@@ TRATANDO DE HACER LISTA DE TODOS LOS SUBFOLDERS ENCONTRADOS.
-# subfiles = [e.name for e in os.scandir(path+(files))]
-# print(subfiles)
-    
 
+"""
+
+
+# @@@@@@@ TRATANDO DE HACER LISTA DE TODOS LOS SUBFOLDERS ENCONTRADOS.
+"""
+subfiles = [e.name for e in os.scandir('../../../module-2')]
+print(subfiles)
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
 
 
 
@@ -104,6 +108,9 @@ print(files)
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+"""
+# @@@@@@@@@@@@ I SHOULD MAKE THIS LIST COMPREHENSIONS MORE SIMPLE... MAYBE TRY A FUNCTIOn
+"""
 random_lists = [
     [e for e in [i for i in random.sample(range(100), 10)]],
     [e for e in [i for i in random.sample(range(100), 10)]],
@@ -113,12 +120,17 @@ random_lists = [
 print(random_lists)
 
 
+
+
+
+
+
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
-
-
+flatten_list = [number for eachlist in list_of_lists for number in eachlist]
+print(flatten_list)
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -127,58 +139,86 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
-
-
+floats = [float(e) for lists in list_of_lists for e in lists]
+print(floats)
 
 #14. Handle the exception thrown by the code below by using try and except blocks. 
 
-"""
-for i in ['a','b','c']:
-    print(i**2)
+try:
+    for i in ['a','b','c']:
+        print(i**2)
+except Exception as e:
+    print('an error ocurred!! details below:')
+    print(type(e), e)
+
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
 #Then use a finally block to print 'All Done.'
 # Check in provided resources the type of error you may use. 
 
-x = 5
-y = 0
-
-z = x/y
-
-
+try:
+    x = 5
+    y = 0
+    z = x/y
+except Exception as e:
+    print('an error ocurred!! details below:')
+    print(type(e), e)
+finally:
+    print('All Done')
 
 
 #16. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
-abc=[10,20,20]
-print(abc[3])
-
+try:
+    abc=[10,20,20]
+    print(abc[3])
+except Exception as e:
+    print('an error ocurred!! details below:')
+    print(type(e), e)
 
 #17. Handle at least two kind of different exceptions when dividing a couple of numbers provided by the user. 
 # Hint: take a look on python input function. 
 # Check in provided resources the type of error you may use. 
 
+print('welcome to the division software')
+while True:
+    numerator = input('What number do you want as the numerator? ')
+    denominator =  input('What number do you want as the denominator? ')
+    try:
+        divided =  float(numerator) / float(denominator)
+        break
+    except ZeroDivisionError as e:
+        print(e, 'you cant divide by 0')
+    except TypeError as e:
+        print(e, 'you can only divide numbers')
+    except Exception as e:
+        print(type(e), e, "@@ Big error !!")
 
-
+print('the result of your division is:', divided)
 
 #18. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
-f = open('testfile','r')
-f.write('Test write this')
-
-
-
+try:
+    f = open('testfile','r')
+    f.write('Test write this')
+except Exception as e:
+    print('an error ocurred!! details below:')
+    print(type(e), e)
 
 #19. Handle the exceptions that can be thrown by the code below using try and except blocks. 
 #Hint: the file could not exist and the data could not be convertable to int
 
-fp = open('myfile.txt')
-line = f.readline()
-i = int(s.strip())
-
+try:
+    fp = open('myfile.txt')
+    line = f.readline()
+    i = int(s.strip())
+except FileNotFoundError as e:
+    print(type(e), e)
+except TypeError as e:
+    print(type(e), e)
 
 
 
@@ -188,9 +228,16 @@ i = int(s.strip())
 # You will probably need to import sys 
 
 def linux_interaction():
-    assert ('linux' in sys.platform), "Function can only run on Linux systems."
+    try:
+        assert ('linux' in sys.platform), "Function can only run on Linux systems."
+        print('yes!! you are using a linux system')
+    except AssertionError as e:
+        print(type(e), e)
+        print(f"you are running a {sys.platform} system. try running on a different system")
     print('Doing something.')
 
+linux_interaction()
+"""
 
 # Bonus Questions:
 
@@ -201,7 +248,17 @@ def linux_interaction():
 # Use a while loop with a try,except, else block to account for incorrect inputs.
 
 
+def squared():
+    while True:
+        number = input('gimmi an integer:')
+            try:
+                int(number)
+            
+    print(number)
 
+squared()
+
+"""
 
 # 22. Find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9). 
 # Use results as the name of the list 
