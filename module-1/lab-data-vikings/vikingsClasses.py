8beaff28951fe94ca483ef889d6f4f45d7016b8b
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-03-27 18:41:47 +0100

    implemented SAXON class completely

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -33,8 +33,19 @@ class Viking(Soldier):
         return "Odin Owns You All!"
 
 # Saxon
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+
+    def attack(self):
+        return super().attack()
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"
 
 
 
