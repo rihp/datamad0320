d4f8e142419d67f26de760c2733ed6f5a9358e00
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-03-27 17:31:44 +0100

    including more function changes for the Viking class

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -10,16 +10,26 @@ class Soldier:
 
     def receiveDamage(self, damage):
         self.health -= damage
-        
-
-
-
-
+    
 # Viking
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+
+    def attack(self):
+        super().attack()
+
+    def receiveDamage(self, damage):
+        super().receiveDamage()
+        if self.health > 0:
+            return f"{name} has received {damage} points of damage"
+        else:
+            return f"{name} has died in act of combat"
 
 
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 class Saxon:
