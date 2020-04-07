4f0086986fe4bde2b955cc2e0e2107486cfcda1d
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-07 15:49:44 +0200

    working on exercises

---
+++ module-2/lab-bash/exercices/bash.sh
@@ -0,0 +1,42 @@
+ 
+
+#    Imprime en consola Hello World.
+echo 'Hello World'
+#    Crea un directorio nuevo llamado new_dir.
+mkdir new_dir
+#    Elimina ese directorio.
+rm -rf new_dir
+#    Copia el archivo sed.txt dentro de la carpeta lorem a la carpeta lorem-copy.
+cp sed.txt ../lorem-copy/sed.txt
+#    Copia los otros dos archivos de la carpeta lorem a la carpeta lorem-copy en una sola línea mediante ;.
+cp at.txt ../lorem-copy/; cp lorem.txt ../lorem-copy/
+#    Mruestra el contenido del archivo sed.txt dentro de la carpeta lorem.
+
+#    Muestra el contenido de los archivos at.txt y lorem.txt dentro de la carpeta lorem.
+
+#    Visualiza las primeras 3 líneas del archivo sed.txt dentro de la carpeta lorem-copy
+
+#    Visualiza las ultimas 3 líneas del archivo sed.txt dentro de la carpeta lorem-copy
+
+#    Añade Homo homini lupus. al final de archivo sed.txt dentro de la carpeta lorem-copy.
+
+#    Visualiza las últimas 3 líneas del archivo sed.txt dentro de la carpeta lorem-copy. Deberías ver ahora Homo homini lupus..
+
+#    Sustituye todas las apariciones de et por ET del archivo at.txt dentro de la carpeta lorem-copy. Deberás usar sed.
+
+#    Encuentra al usuario activo en el sistema.
+
+#    Encuentra dónde estás en tu sistema de ficheros.
+
+#    Lista los archivos que terminan por .txt en la carpeta lorem.
+
+#    Cuenta el número de líneas que tiene el archivo sed.txt dentro de la carpeta lorem.
+
+#    Cuenta el número de archivos que empiezan por lorem que están en este directorio y en directorios internos.
+
+#    Encuentra todas las apariciones de et en at.txt dentro de la carpeta lorem.
+
+#   Cuenta el número de apariciones del string et en at.txt dentro de la carpeta lorem.
+
+#    Cuenta el número de apariciones del string et en todos los archivos del directorio lorem-copy.
+
