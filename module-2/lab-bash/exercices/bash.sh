c5522d0d18ae4d92183af5af1e3d2a2512300532
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-07 18:51:45 +0200

    finished bonus questions

--- module-2/lab-bash/exercices/bash.sh
+++ module-2/lab-bash/exercices/bash.sh
@@ -23,7 +23,7 @@ less lorem/sed.txt # opens and tries to read
 cat lorem/at.txt ; cat lorem/lorem.txt
 
 #    Visualiza las primeras 3 líneas del archivo sed.txt dentro de la carpeta lorem-copy
- cat lorem-copy/sed.txt | head -n3
+cat lorem-copy/sed.txt | head -n3
 
 #    Visualiza las ultimas 3 líneas del archivo sed.txt dentro de la carpeta lorem-copy
 cat lorem-copy/sed.txt | tail -n3
@@ -41,7 +41,7 @@ sed -i 's/et/ET/g' lorem-copy/at.txt
 
 #    Encuentra al usuario activo en el sistema.
 who
-w # more detailed output
+w     # more detailed output
 
 #    Encuentra dónde estás en tu sistema de ficheros.
 pwd
@@ -69,3 +69,58 @@ grep -o et lorem-copy/* # prints a line for each match, specifying each file on 
 grep -o -i et lorem-copy/*  # same as above but ignores the upper and lowercase
 grep -o et lorem-copy/* | wc -l # pipeline to print the NUMBER of ocurrences
 grep -o -i et lorem-copy/* | wc -l # same as above but ignores the upper and lowercase
+
+   #####################
+ ######################
+## Bonus
+#
+#   Almacena en una variable name tu nombre.
+NAME='ROBERTO' 
+# NAME = 'ROBERTO' # NO VA A FUNCIONAR CON ESPACIOS
+
+#    Imprime esa variable.
+echo $ROBERTO
+
+#     Crea un directorio nuevo que se llame como el contenido de la variable name.
+mkdir $NAME
+
+#     Elimina ese directorio.
+rm -rf $NAME
+
+#     Por cada archivo dentro de la carpeta lorem imprime el número de carácteres que tienen sus nombres. Intenta primero mostrar los archivos mediante un bucle for
+#         Imprime los ficheros
+for f in lorem/*;  do echo $f; done  # Imprime la ruta a cada archivo en la carpeta lorem/*
+#         Imprime las longitudes de los nombres de los ficheros
+cd lorem; for f in *; do echo ${#f}; done; cd ..
+#         Imprime outputs con la siguiente estructura: lorem has 5 characters lenght
+cd lorem; for f in *; do echo $f 'has '${#f} 'characters length'; done; cd ..
+
+
+#     Muestra los procesos de forma jerárquica que se están ejecutando en tu ordenador:
+#         Usando el comando top o htop
+top
+#         Usando el comando ps con argumentos
+ps
+
+#     Muestra información sobre tu procesador por pantalla
+cat /proc/cpuinfo
+cat /proc/cpuinfo | grep 'core id' # count how many cores in processor
+lscpu
+lshw
+# more: https://www.binarytides.com/linux-cpu-information/
+
+#     Crea 3 alias y haz que estén disponibles cada vez que inicias sesión
+# https://linuxize.com/post/how-to-create-bash-aliases/
+echo alias python='python3' >> ~/.bashrc
+echo alias pip='pip3' >> ~/.bashrc
+echo alias tgz='tar -czvf' >> ~/.bashrc
+# OPTIONAL :
+# source ~/.bash_profile # changes the bash profile from whatever it was and uses ~/.bash_profile
+
+#     Comprime las carpetas lorem y lorem-copy en un archivo llamado lorem-compressed.tar.gz
+tar -czvf lorem-compressed.tar.gz lorem/ lorem-copy/
+# OPTIONAL (using new alias): 
+# tgz lorem-compressed.tar.gz lorem/ lorem-copy/
+
+#     Descomprime el archivo lorem-compressed.tar.gz en la carpeta lorem-uncompressed
+FOLDER='lorem-uncompressed'; mkdir $FOLDER; tar -xzvf lorem-compressed.tar.gz -C $FOLDER/
